d82df6a6a3ed4d7e7be2e22d2f7ff24391ecfd30
Alex Deane <alexdeane16@gmail.com>
2024-08-28 13:47:34 +0000

    Initial commit

--- index.js
+++ index.js
@@ -5,6 +5,7 @@
 var express = require('express');
 var app = express();
 
+
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
@@ -18,12 +19,37 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+//Date function checker
+
+const isInvalidDate = (date) => date.toUTCString() === "Invalid Date";
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/:date", function (req, res) {
+  //Create a variable that sets the date as new date
+  let date = new Date(req.params.date); //Has to match up with date in app.get method
+  //Check if date is valid
+  if (isInvalidDate(date)) {
+    date = new Date(+req.params.date);
+  }
+
+  if(isInvalidDate(date)) {
+    res.json({error: "Invalid Date"})
+    return; //Need to return so that the rest of the code doesn't run
+  }
+
+  res.json({
+    unix: date.getTime(),
+    utc: date.toUTCString()
+  });
 });
 
+
+app.get("/api/", function (req, res) {
+  res.json({
+    unix: new Date().getTime(),
+      utc: new Date().toUTCString()
+    })
+  })
 
 
 // Listen on port set in environment variable or default to 3000
